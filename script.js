7dcd73162621ff351ee7f5d503d40fb665b42d9f
Jomerjom <ekenesjoachim@gmail.com>
2023-11-29 11:04:32 +0100

    Changed to lower res pirctures to fix loadtimes

--- script.js
+++ script.js
@@ -3,61 +3,61 @@ const peopleArray = [
       name: "John Doe",
       age: 25,
       hobbies: ["Reading", "Hiking"],
-      img: "images/m1.webp"
+      img: "images/m1.jpg"
     },
     {
       name: "Jane Smith",
       age: 30,
       hobbies: ["Painting", "Traveling"],
-      img: "images/f1.webp"
+      img: "images/f1.jpg"
     },
     {
       name: "Bob Johnson",
       age: 22,
       hobbies: ["Coding", "Gaming"],
-      img: "images/m2.webp"
+      img: "images/m2.jpg"
     },
     {
       name: "Alice Brown",
       age: 28,
       hobbies: ["Cooking", "Photography"],
-      img: "images/f2.webp"
+      img: "images/f2.jpg"
     },
     {
       name: "David Wilson",
       age: 35,
       hobbies: ["Running", "Music"],
-      img: "images/m3.webp"
+      img: "images/m3.jpg"
     },
     {
       name: "Emily Davis",
       age: 26,
       hobbies: ["Dancing", "Yoga"],
-      img: "images/f3.webp"
+      img: "images/f3.jpg"
     },
     {
       name: "Chris Miller",
       age: 32,
       hobbies: ["Fishing", "Camping"],
-      img: "images/m4.webp"
+      img: "images/m4.jpg"
     },
     {
       name: "Sophia Garcia",
       age: 29,
       hobbies: ["Writing", "Reading"],
-      img: "images/f4.webp"
+      img: "images/f4.jpg"
     },
     {
       name: "Michael Martinez",
       age: 27,
       hobbies: ["Swimming", "Playing Guitar"],
-      img: "images/m5.webp"
+      img: "images/m5.jpg"
     },
     {
       name: "Olivia Robinson",
       age: 31,
       hobbies: ["Skiing", "Travelling"],
-      img: "images/f5.webp"
+      img: "images/f5.jpg"
     }
   ];
 
